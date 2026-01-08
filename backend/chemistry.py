2f63e1ab7e52ad9f4b3443ca7bb031c37813f2f2
cynthia-wen <68306562+cynthia-wen@users.noreply.github.com>
2026-01-08 21:22:36 +0000

    stole smiles_to_svg function, added functions for SA score and QED score

--- backend/chemistry.py
+++ backend/chemistry.py
@@ -1,1 +1,30 @@
-print("hello world")
+from rdkit import Chem
+from rdkit.Chem import QED
+from rdkit.Contrib.SA_Score import sascorer
+
+# took from demo
+def smiles_to_svg(smiles: str, width: int = 400, height: int = 400) -> bytes:
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        raise RuntimeError("Invalid SMILES")
+
+    Chem.rdCoordGen.AddCoords(mol)
+    drawer = Chem.Draw.rdMolDraw2D.MolDraw2DSVG(width, height)
+    # set drawing options on drawer.getOptions()
+    drawer.DrawMolecule(mol)
+    drawer.FinishDrawing()
+    return drawer.GetDrawingText().encode()
+
+def get_synthesis_score(smiles: str):
+    mol = Chem.MolFromSmiles(smiles) # Convert SMILES to RDKit Molecule object
+    if mol:
+        score = sascorer.calculateScore(mol) # Calculate the synthetic accessibility score
+        return round(score, 2)
+    return None
+
+def calculate_drug_likeness(smiles: str):
+    mol = Chem.MolFromSmiles(smiles)
+    if mol:
+        score = QED.qed(mol) # Calculate the QED score
+        return round(score, 3)
+    return None
